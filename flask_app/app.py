db7283f67dcd3649f98cc02b6584879733dc36be
NitinNandeshwar <samarthtnitin64@gmail.com>
2025-06-29 19:33:03 +0100

    CI/CD run - 7

--- flask_app/app.py
+++ flask_app/app.py
@@ -69,8 +69,8 @@ def normalize_text(text):
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/NitinNandeshwar/MLOPs_Project1.mlflow')
-dagshub.init(repo_owner='NitinNandeshwar', repo_name='MLOPs_Project1', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/NitinNandeshwar/MLOPs_Project1.mlflow')
+# dagshub.init(repo_owner='NitinNandeshwar', repo_name='MLOPs_Project1', mlflow=True)
 # -------------------------------------------------------------------------------------
 
 # Below code block is for production use
